4a655c433d4b20d523b3bd5c7c38985770a5bf7b
Andrei Kretov <akretov@student.42prague.com>
2025-01-15 19:29:57 +0100

    still need to work on reference and pointer when dealing across 2 classes

---
+++ ex03/Weapon.cpp
@@ -0,0 +1,17 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type) : _type(type) {}
+
+Weapon::~Weapon(void) {
+    std::cout << "World no longer needs violance" << std::endl;
+}
+
+const std::string &Weapon::getType(void) {
+    return (_type);
+}
+
+void    Weapon::setType (std::string new_type) {
+    std::cout << "Evolution happened from " << _type 
+                << " to " << new_type << std::endl;
+    _type = new_type;
+}
